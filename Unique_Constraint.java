89f6fe2df0978e8fa9ef7dd7c09215ad758996c6
N-ADA <95967451+N-ADA@users.noreply.github.com>
2022-07-25 09:29:43 +0100

    Add files via upload

---
+++ Unique_Constraint.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.minigoldengate;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author nadabounajma
+ */
+public class Unique_Constraint {
+    private String constraint;
+    private ArrayList<String> columns;
+    
+    public Unique_Constraint(String constraint, ArrayList<String> columns){
+        this.constraint = constraint;
+        this.columns = columns;
+    }
+
+    public String getConstraint() {
+        return constraint;
+    }
+
+    public ArrayList<String> getColumns() {
+        return columns;
+    } 
+    
+}
